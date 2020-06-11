3d4d6390cecfb3b5359f06504271d9785c9c17f4
benjaminngarambe <63362925+benjaminngarambe@users.noreply.github.com>
2020-06-11 21:42:12 +0200

    Add files via upload

---
+++ Project05/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Project05
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string[] inputs;
+            inputs = Console.ReadLine().Split(' ');
+
+            int L = int.Parse(inputs[1]); // the number of links
+            int E = int.Parse(inputs[2]); // the number of exit gateways
+            List<Connection> links = new List<Connection>();
+            for (int x = 0; x < L; x++)
+            {
+                inputs = Console.ReadLine().Split(' ');
+                links.Add(new Connection(int.Parse(inputs[0]), int.Parse(inputs[1])));
+            }
+            List<int> gateways = new List<int>();
+            for (int x = 0; x < E; x++)
+                gateways.Add(int.Parse(Console.ReadLine()));
+
+            // game loop
+            while (true)
+            {
+                int SI = int.Parse(Console.ReadLine()); // The index of the node on which the Skynet agent is positioned this turn
+
+                Connection.CloseGateway(SI, gateways, links);
+            }
+        }
+
+        public class Connection
+        {
+            //declaring the private member integers
+            private int Vertex1 { get; set; }
+
+            private int Vertex2 { get; set; }
+
+            public Connection(int vertex1, int vertex2)
+            {
+                this.Vertex1 = vertex1;
+                Vertex2 = vertex2;
+            }
+
+            public override string ToString()
+            {
+                return $"{Vertex1} {Vertex2}";
+            }
+
+            public override bool Equals(object obj)
+            {
+                //casting link as connection
+                Connection link = obj as Connection;
+                return (Vertex1 == link.Vertex1 && Vertex2 == link.Vertex2) || (Vertex1 == link.Vertex2 && Vertex2 == link.Vertex1);
+            }
+
+            static public void CloseGateway(int agentIndex, List<int> gateways, List<Connection> links)
+            {
+                foreach (int gateway in gateways)
+                {
+                    Connection link = new Connection(agentIndex, gateway);
+                    if (links.Contains(link))
+                    {
+                        Console.WriteLine(link);
+                        links.Remove(link);
+                        return;
+                    }
+                }
+                Console.WriteLine(links[new Random().Next(0, links.Count)]);
+            }
+        }
+    }
+}
---
+++ Project05/Readme.md
@@ -0,0 +1,50 @@
+<a href="https://www.instagram.com/ngarambe_benjamin/">
+  <img src="benjamin Logo.png" alt="HTML tutorial" >
+</a>
+
+<a href="https://www.instagram.com/ngarambe_benjamin/">
+  <img src="CodinGame logo.svg.png" alt="Coding Game" >
+</a>
+
+#  Skynet Revolution - Episode 1
+The Goal
+
+Your virus has caused a backdoor to open on the Skynet network enabling you to send new instructions in real time.
+
+You decide to take action by stopping Skynet from communicating on its own internal network.
+
+Your mission is to reprogram the virus so it will sever links in such a way that the Skynet Agent is unable to access another sub-network thus preventing information concerning the presence of our virus to reach Skynet's central hub.
+## Rules
+For each test you are given:
+<ul>
+<li>A map of the network.</li>
+<li>The position of the exit gateways.</li>
+<li>The starting position of the Skynet agent.</li>
+</ul>
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update the tests as appropriate.
+
+## License
+Copyright (c) 2020 Benjamin Ngarambe
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
